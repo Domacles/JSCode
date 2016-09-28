8ea36b1dbb94db5765c52b6c1b1c1acd5e4460f3
MIsike <domacles0124@gmail.com>
2016-09-28 21:29:31 +0800

    update

--- chatrooms/lib/chat_server.js
+++ chatrooms/lib/chat_server.js
@@ -2,7 +2,7 @@ var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
 var nickNames = {};
-var namesUsed = {};
+var namesUsed = [];
 var currentRoom = {};
 
 exports.listen = function (server) {
