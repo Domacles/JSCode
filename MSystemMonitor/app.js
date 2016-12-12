d10ac4badd37528b35f6e759e7b61615b3240af6
MIsike <domacles0124@gmail.com>
2016-12-12 21:17:11 +0800

    update config and use tsc

--- MSystemMonitor/app.js
+++ MSystemMonitor/app.js
@@ -1,33 +1,25 @@
-const {app, BrowserWindow} = require('electron')
-
-let win
-
-function createWindow () {
-  
-  win = new BrowserWindow({width: 800, height: 600})
-
-  win.loadURL(`file://${__dirname}/app.html`)
-
-  win.webContents.openDevTools()
-
-  // 处理窗口关闭
-  win.on('closed', () => {
-    win = null
-  })
+const { app, BrowserWindow } = require('electron');
+let win;
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadURL(`file://${__dirname}/app.html`);
+    win.webContents.openDevTools();
+    // 处理窗口关闭
+    win.on('closed', () => {
+        win = null;
+    });
 }
-
 // Electron初始化完成
-app.on('ready', createWindow)
-
+app.on('ready', createWindow);
 // 处理退出
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
-})
+    if (win === null) {
+        createWindow();
+    }
+});
+//# sourceMappingURL=app.js.map
