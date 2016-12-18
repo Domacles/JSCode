b5ca1ae63460f66b49e4a324e66b9431c3e97be3
MIsike <domacles0124@gmail.com>
2016-12-18 16:40:54 +0800

    update

--- MSystemMonitor/app.js
+++ MSystemMonitor/app.js
@@ -1,6 +1,16 @@
 "use strict";
 require("source-map-support/register");
+//module
+const electron = require("electron");
 const child_process = require("child_process");
 //function & class
 const exec = child_process.exec;
+const BrowserWindow = electron.BrowserWindow;
+//var & let
+let cmd = process.platform == 'win32' ? 'tasklist' : 'ps aux';
+//custom function & class
+class Application {
+}
+;
+//main_script
 //# sourceMappingURL=app.js.map
--- MSystemMonitor/app.ts
+++ MSystemMonitor/app.ts
@@ -5,8 +5,46 @@ import * as fs from 'fs';
 import * as child_process from 'child_process';
 //function & class
 const exec: typeof child_process.exec = child_process.exec;
+const BrowserWindow: typeof electron.BrowserWindow = electron.BrowserWindow;
 //var & let
-let cmd: String = process.platform == 'win32' ? 'tasklist' : 'ps aux';
 //custom function & class
+class Application {
+    private app: Electron.App;
+    private mainWindow: Electron.BrowserWindow;
 
+    protected createWindow() {
+        if (this.mainWindow === null)
+            this.mainWindow = new BrowserWindow({ width: 800, height: 600 });
+        this.mainWindow.setTitle('MSystemMonitor');
+        this.mainWindow.loadURL(`file://${__dirname}/app.html`);
+        this.mainWindow.on('closed', () => {
+            this.mainWindow = null;
+        });
+    }
+
+    protected appQuit() {
+        if (process.platform !== 'darwin')
+            this.app.quit();
+    }
+
+    protected appActivate() {
+        if (this.mainWindow === null)
+            this.createWindow();
+    }
+
+    constructor(app = electron.app) {
+        this.app = app;
+        this.mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+    }
+
+    run() {
+        let cmd: String = process.platform == 'win32' ? 'tasklist' : 'ps aux';
+        this.app.on('ready', this.createWindow);
+        this.app.on('window-all-closed', this.appQuit);
+        this.app.on('activate', this.appActivate);
+    }
+};
 //main_script
+let mainAppLication = new Application();
+mainAppLication.run();
