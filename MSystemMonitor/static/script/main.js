00034e875481b5cbcef6ffc0c657c2a881eea517
MIsike <domacles0124@gmail.com>
2016-12-29 14:44:15 +0800

    update

--- MSystemMonitor/static/script/main.js
+++ MSystemMonitor/static/script/main.js
@@ -1,3 +1,6 @@
 //import React and ReactDOM
 "use strict";
+const React = require("react");
+const ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("h1", null, "Hello, world!"), document.getElementById('example'));
 //# sourceMappingURL=main.js.map
